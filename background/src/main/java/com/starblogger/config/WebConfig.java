4d0805c77cc9bed1e2b4b955d9d52aa119f7ead7
yayangy <2903962059@qq.com>
2025-10-06 19:46:24 +0800

    更新

--- background/src/main/java/com/starblogger/config/WebConfig.java
+++ background/src/main/java/com/starblogger/config/WebConfig.java
@@ -19,7 +19,9 @@ public class WebConfig {
                                 "http://localhost:5173",
                                 "http://127.0.0.1:5173",
                                 "http://localhost:4173",
+                                "http://47.98.137.119:5173",
                                 "http://127.0.0.1:4173")
+
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
